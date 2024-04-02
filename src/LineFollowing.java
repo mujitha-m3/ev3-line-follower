edf32902a9ca635b413cb5f5ab7b7a69edc0be40
AsithaSandaru <143335815+AsithaSandaru@users.noreply.github.com>
2024-04-02 23:12:03 +0300

    Set motor speeds

--- src/LineFollowing.java
+++ src/LineFollowing.java
@@ -19,8 +19,8 @@ public class LineFollowing {
 
 
         // Set motor speeds
-        int baseSpeed = 300;
-        int searchSpeed = baseSpeed / 2; // Reduced speed for searching
+        Int baseSpeed = 200;
+        Int searchSpeed = baseSpeed / 1; // Reduced speed for searching
         leftMotor.setSpeed(baseSpeed);
         rightMotor.setSpeed(baseSpeed);
 
