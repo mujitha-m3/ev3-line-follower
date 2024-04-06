3a00ac620576ec3027df5e0995723644939b8acc
kcdissanayaka <kc.dissanayaka@outlook.com>
2024-04-06 12:03:56 +0300

    Create Time Tracker Thread and also Update program obstacle detection method

--- src/ObstacleDetectionThread.java
+++ src/ObstacleDetectionThread.java
@@ -1,13 +1,18 @@
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 
 public class ObstacleDetectionThread extends Thread {
     private EV3UltrasonicSensor ultrasonicSensor;
     private boolean obstacleDetected;
+    private EV3LargeRegulatedMotor leftMotor;
+    private EV3LargeRegulatedMotor rightMotor;
 
-    public ObstacleDetectionThread(EV3UltrasonicSensor ultrasonicSensor) {
+    public ObstacleDetectionThread(EV3UltrasonicSensor ultrasonicSensor, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor) {
         this.ultrasonicSensor = ultrasonicSensor;
         this.obstacleDetected = false;
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
     }
 
     @Override
@@ -22,6 +27,7 @@ public class ObstacleDetectionThread extends Thread {
 
             if (distance < 10) {
                 setObstacleDetected(true);
+                LineFollowing.notifyObstacleDetected(); // Notify LineFollowing class about obstacle
             } else {
                 setObstacleDetected(false);
             }
